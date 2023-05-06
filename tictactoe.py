548b04ad0547a506ff2cfb690894e3699b2dafb6
altinabytyqi <94831473+altinabytyqi@users.noreply.github.com>
2023-05-06 21:50:17 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -16,7 +16,8 @@ class TicTacToe:
         if event in (sg.WIN_CLOSED, 'Exit'):
             return None
         
-    def new_game(self, adversary_type=1, first_player=1, x_cells=3, y_cells=3, is_swap2=False, player_names=[]):
+    def new_game(self, adversary_type=1, first_player=1, x_cells=3, y_cells=3, is_swap2=False,
+                 player_names=["x", "o"]):
         print(first_player)
         self.x_cells = x_cells
         self.y_cells = y_cells
@@ -29,6 +30,27 @@ class TicTacToe:
             self.player_names[1] = "computer. Loading move"
         self.init_table()
         self.init_graphics()
+
+        self.window = sg.Window('TicTacToe', self.layout, element_padding=((0, 0), (0, 0)), margins=(0, 0))
+        if is_swap2:
+            if adversary_type == 0:  # human
+                self.swap_moves(first_player, 3)
+                if values[1]:
+                    self.player_names = self.player_names[::-1]
+                if values[2]:
+                    self.player_to_put_piece = (self.player_to_put_piece + 1) % 2
+                    self.window['the_current_player'].update(
+                        f"Player at turn: {self.player_names[self.player_to_put_piece]}")
+                    self.swap_moves(first_player * -1, 2)
+                    if values[1]:
+                        self.player_names = self.player_names[::-1]
+
+                self.player_to_put_piece = (self.player_to_put_piece + 1) % 2
+                self.window['the_current_player'].update(
+                    f"Player at turn: {self.player_names[self.player_to_put_piece]}")
+                self.next_click(first_player * -1)
+            else:
+                                 
 
 if __name__ == '__main__':
     Game = TicTacToe()
